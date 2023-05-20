9d3c8fdff828c8f305a1607d80a5c6a2a432a2b1
Ana <ana.pereirar@utadeo.edu.co>
2023-05-19 20:57:13 -0500

    Filmoteka

---
+++ src/js/team.js
@@ -0,0 +1,58 @@
+export const members = [
+    {
+      "name": "Dimitr",
+      "surname": "Remicio",
+      "url": "https://github.com/Dimitr-Remicio",
+      "role": ["Team Lead", "Web Developer"],
+      "photo": "https://avatars.githubusercontent.com/u/113401276?v=4"
+    },
+    {
+      "name": "Edison",
+      "surname": "Restrepo",
+      "url": "https://github.com/EdiRestrepo",
+      "role": ["Web Developer"],
+      "photo": "https://avatars.githubusercontent.com/u/107229020?v=4"
+    },
+    {
+      "name": "Emmanuel",
+      "surname": "Giraldo",
+      "url": "https://github.com/esgg1",
+      "role": ["Web Developer"],
+      "photo": "https://avatars.githubusercontent.com/u/115299104?v=4"
+    },
+    {
+      "name": "Ana",
+      "surname": "Pereira",
+      "url": "https://github.com/anapereirar",
+      "role": ["Web Developer"],
+      "photo": "https://avatars.githubusercontent.com/u/64859363?v=4"
+    },
+    {
+      "name": "Santiago",
+      "surname": "Valencia",
+      "url": "https://github.com/Santiagovalb",
+      "role": ["Web Developer"],
+      "photo": "https://avatars.githubusercontent.com/u/115671492?v=4"
+    },
+    {
+      "name": "Diego",
+      "surname": "Villamizar",
+      "url": "https://github.com/Arq0390",
+      "role": ["Web Developer"],
+      "photo": "https://avatars.githubusercontent.com/u/115050140?v=4"
+    },
+    {
+      "name": "Luis",
+      "surname": "Tegua",
+      "url": "https://github.com/LuisTegua",
+      "role": ["Web Developer"],
+      "photo": "https://avatars.githubusercontent.com/u/116098603?v=4"
+    },
+    {
+      "name": "Alejandra",
+      "surname": "Centellas",
+      "url": "https://github.com/alecente",
+      "role": ["Web Developer"],
+      "photo": "https://avatars.githubusercontent.com/u/110432497?v=4"
+    }
+  ]
